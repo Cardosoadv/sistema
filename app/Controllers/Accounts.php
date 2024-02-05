329a1c6a08f98ec1b1cfde4e505a264072a6e46d
Cardosoadv <fabianocardoso.adv@gmail.com>
2024-02-05 17:28:36 -0300

    iniciando contas

---
+++ app/Controllers/Accounts.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use App\Models\ClientsModel;
+use DateTime;
+
+class Accounts extends BaseController
+{
+    //função para formatar a data. Ainda não foi testada.
+    protected function novaData($data)
+    {
+    $novaData = date_format(new DateTime($data), 'Y-m-d');
+    return $novaData;
+    }
+
+    public function index()
+    {
+        $ClientsModel = new ClientsModel();
+        $data['clients'] = $ClientsModel
+            ->get()->getResultArray();
+
+        return  view('clients', $data);
+    }
+
+    public function get_client($id)
+    {
+        $ClientsModel = new ClientsModel();
+        $data = $ClientsModel->where('id', $id)->first();
+               return $this->response->setJSON($data);
+    }
+
+
+    public function adicionar()
+    {
+        $ClientsModel = new ClientsModel();
+
+        $data['name']         =  $this->request->getPost('name');
+        $data['celular']      =  $this->request->getPost('celular');
+        $data['email']      =  $this->request->getPost('email');
+        $data['landed_at']    = $this->novaData($this->request->getPost('landed_at'));
+
+        $ClientsModel->insert($data);
+        return $this->response->redirect(site_url('clients'));
+
+    }
+
+    public function atualizar($id)
+    {
+        $ClientsModel = new ClientsModel();
+        $data = [
+            'name'              =>  $this->request->getPost('name'),
+            'celular'           =>  $this->request->getPost('celular'),
+            'email'             =>  $this->request->getPost('email'),
+            'landed_at'         =>  $this->novaData($this->request->getPost('landed_at'))
+        ];
+        $ClientsModel->update($id,$data);
+        $msg = "Dados atualizados com sucesso!";
+        $session = \Config\Services::session();
+        $session->set($msg);
+        return $this->response->redirect(site_url('clients'));
+    }
+
+    public function delete($id)
+    {
+        $ClientsModel = new ClientsModel();
+        $ClientsModel->delete($id);
+ //       return $this->response->redirect(site_url('clients'));
+
+    }
+
+
+}
